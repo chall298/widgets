b22de28070837224184788bd2263c7908462506b
chall298 <chall361@gmail.com>
2021-01-28 13:00:51 -0500

    Translation app is working

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,5 +1,6 @@
 import React,{ useState } from 'react';
 import Dropdown from './Dropdown';
+import Convert from './Convert';
 
 const options = [
     { 
@@ -13,19 +14,34 @@ const options = [
     {
         label: 'Hindi',
         value: 'hi'
+    },
+    {
+        label: 'Dutch',
+        value: 'nl'
     }
 ];
 
 const Translate = () => {
     const [language, setLanguage] = useState(options[0]);
+    const [text, setText] = useState('');
 
     return (
         <div>
-            <Dropdown 
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input value={text} onChange={(e) => setText(e.target.value)} />
+                </div>
+            </div>
+            <Dropdown
+                label="select a language"
                 selected={language} 
                 onSelectedChange={setLanguage} 
                 options={options}
             />
+            <hr />
+            <h3 className="ui header">Output</h3>
+            <Convert text={text} language={language} />
         </div>
     )
 };
